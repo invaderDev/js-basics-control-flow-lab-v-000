20863adb0f09921fe91f10cf9ccddd0529fe1756
Kris Thompson <kt.devcat@gmail.com>
2017-12-29 20:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   if (feet < 400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   }
   else if (feet > 2000 && feet < 2500) {
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   }
   else if (feet >= 2500) {
-    return 'No can do.'
+    return 'No can do.';
   }
+}
+
+function ternaryCheckCity (city) {
+  city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
